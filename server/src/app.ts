40bdb5c3b2778634f6a01879d9c2aede9bea88c7
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2026-07-06 02:18:28 +0530

    style: format code for consistency and readability

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,18 @@
 import express from "express";
-import type { Request, Response } from "express"
-import { globalErrorHandler, notFoundHandler } from "./middleware/globalErrorHandler";
-import todoRouter from "./routes/todo"
-
+import type { Request, Response } from "express";
+import {
+  globalErrorHandler,
+  notFoundHandler,
+} from "./middleware/globalErrorHandler";
+import todoRouter from "./routes/todo";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/health", (_req: Request, res: Response) => {
-    res.status(201).json({ message: "Api server working fine" })
-})
-
+  res.status(201).json({ message: "Api server working fine" });
+});
 
 app.use("/api/todos", todoRouter);
 
