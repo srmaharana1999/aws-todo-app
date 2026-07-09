b314807d587c7cd107d890e065a37c0ccb075bb4
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2026-07-10 00:30:22 +0530

    chore: add console log for database URL in prisma.ts

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -6,6 +6,8 @@ import { PrismaClient } from "../generated/prisma/client.js";
 
 const connectionString = config.dbUrl;
 
+console.log("DB URL:", connectionString);
+
 const adapter = new PrismaPg({ connectionString });
 const prismaClient = new PrismaClient({ adapter });
 
